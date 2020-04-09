8ca67fcebe991f7685fc0fc06eafd9759e299f47
Martin Sebastian Hailant <53666734+Jailant@users.noreply.github.com>
2020-04-09 04:55:20 -0300

    Update index.js

--- hostname/index.js
+++ hostname/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const hostname = os.hostname();
 
 app.get('/', (req, res) => {
-  res.send(`Saludos desde ${hostname}!`);
+  res.send(`Saludos de Martin desde ${hostname}!`);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
